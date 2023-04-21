f1122e8285487353bbefacacbbc7d620f62d7b09
rwirba <82109085+rwirba@users.noreply.github.com>
2023-04-21 16:00:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,30 +2,9 @@ provider "aws" {
   region = var.region
 }
 
-# Create a new VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = var.vpc_name
-  }
-}
-
-# Create a new subnet in the VPC
-resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = var.availability_zone
-
-  tags = {
-    Name = var.subnet_name
-  }
-}
-
 # Create a new security group for the instance
 resource "aws_security_group" "my_security_group" {
   name_prefix = var.security_group_name
-  vpc_id      = aws_vpc.my_vpc.id
 
   # Allow traffic on port 22 (SSH), 80 (HTTP), and 8080 (Custom App) from your IP address
   ingress {
@@ -62,21 +41,32 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
-# Create a new EC2 instance in the subnet
-resource "aws_instance" "" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = aws_subnet.my_subnet.id
+resource "aws_instance" "jenkins" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name 
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
-
   tags = {
-    Name = ""
+    Name = "jenkins"
   }
 }
 
-# Outputs
-output "public_ip_jenkins" {
-  value = aws_instance"resource name".public_ip
+resource "aws_instance" "slave" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name             
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  tags = {
+    Name = "slave"
+  }
 }
 
+resource "aws_instance" "myapp" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name             
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  tags = {
+    Name = "myapp"
+  }
+}
